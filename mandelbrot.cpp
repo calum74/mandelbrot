a9b9d74a1d2ba61578775cd02f42e7ce54af5d20
Calum Grant <calumgrant@github.com>
2024-12-24 06:49:10 +0000

    Tidy

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -4,8 +4,9 @@
 #include "fractal.hpp"
 #include "orbit.hpp"
 
-#include <cassert>
-
+// Checks whether the number n has enough precision for the value it's holding.
+// We'll use this to decide whether we need to switch to a higher-precision
+// implementation.
 template <int N>
 bool valid_precision(const fractals::high_precision_real<N> &n) {
   for (int i = 0; i < N - 2; ++i) {
@@ -199,43 +200,31 @@ public:
 
     as before. This should give d in terms of d' as
 
-      d' = -id/z
+      d' = id/(c(c+d))
 
     So the basic algorithm is that we transform the central point by
-    -i/c, and transform the delta by -id/c.
+    -i/c, and transform the delta by id/(c(c+d)).
 
-    At a sufficiently small delta, we can assume that the mapping is linear, so
-    we only need to compute delta once per image. Then we can plug the z and d
-    into our original high-performance Mandelbrot algorithms.
+    Then we can plug the c and d into our original high-performance Mandelbrot
+    algorithms.
+
+    Mandeldrop looks like the Mandelbrot set at higher magnification because
+    this is a conformal mapping.
   */
 
   using SmallReal = typename LowPrecisionComplex::value_type;
   using BigReal = typename HighPrecisionComplex::value_type;
 
   static HighPrecisionComplex map(const HighPrecisionComplex &z) {
-
-    // Morally this is
-    // return HighPrecisionComplex{0, -1} / z;
-    // But std::complex does not like this, so instead we have
-
-    // Hack: Just use doubles
-    // TODO: Need an inverse
     auto n = inverse(mandelbrot::norm(z));
-    // assert(convert<double>(mandelbrot::norm(z)) < 100.0);
-    // BigReal n = 1.0 / convert<double>(mandelbrot::norm(z));
     return {-imag(z) * n, -real(z) * n};
   }
 
   LowPrecisionComplex map_delta(const LowPrecisionComplex &d) const {
-    auto n = 1 / norm(c0);
-
+    // Note that we can calculate deltas using low precision (`double`) complex
+    // numbers. We just need to rearrange our calculation of the delta to avoid
+    // loss of precision.
     return LowPrecisionComplex{0, 1} * d / (c0 * (c0 + d));
-
-    // This is accurate but loses precision
-    return LowPrecisionComplex{0, -1} / (c0 + d) +
-           LowPrecisionComplex{0, 1} / c0;
-
-    // return LowPrecisionComplex{0, -1} * d * z; //  / z;
   }
 
   // Initialize the fractal. We calculate the high precision reference orbit
@@ -263,7 +252,7 @@ public:
 
   // The initial coordinates to view the Mandelbrot set.
   // Also specify the number of iterations (500).
-  static ViewCoords initial_coords() { return {-0.5, 0, 2, 500}; }
+  static ViewCoords initial_coords() { return {0, -1, 3, 500}; }
 
   // Calculates a single point of the fractal, at position (x,y).
   // Look up the actual coordinates (or in this case, the delta from the center
@@ -302,6 +291,7 @@ private:
   // Currently always at the center of the image.
   const int ref_x, ref_y;
 
+  // The position of the reference orbit.
   const LowPrecisionComplex c0;
 
   // The calculated reference orbit, together with Taylor series terms for the
