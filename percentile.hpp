ebc06d4c540e20d1bb9f648b6ebc0d08ecc8c552
Calum Grant <calumgrant@github.com>
2024-12-31 06:58:44 +0000

    Percentiles returns an iterator

--- percentile.hpp
+++ percentile.hpp
@@ -2,15 +2,14 @@
 #include <algorithm>
 
 namespace util {
-// Helper to find the 99.9th percentile
-// Technically O(n.ln n) but faster because we only sort the top 0.1% of
-// the data
-template <typename It>
-typename It::value_type percentile(It first, It last, double p) {
+// Helper to find high percentiles (e.g. 99.9%)
+// Technically O(n.ln n) but faster when we're looking for high percentiles
+// because only the top items are sorted.
+template <typename It> It top_percentile(It first, It last, double p) {
   std::make_heap(first, last);
   int to_skip = std::distance(first, last) * (1.0 - p);
   for (int i = 0; i < to_skip; ++i, --last)
     std::pop_heap(first, last);
-  return *last;
+  return last;
 }
 } // namespace util
