4d20b41fde83cd1c9e8d514b0e931908b9f94315
Calum Grant <calumgrant@github.com>
2025-01-08 08:14:34 +0000

    Percentile returns a valid iterator

--- include/percentile.hpp
+++ include/percentile.hpp
@@ -8,7 +8,7 @@ namespace util {
 template <typename It> It top_percentile(It first, It last, double p) {
   std::make_heap(first, last);
   int to_skip = std::distance(first, last) * (1.0 - p);
-  for (int i = 0; i < to_skip; ++i, --last)
+  for (int i = 0; i <= to_skip; ++i, --last)
     std::pop_heap(first, last);
   return last;
 }
