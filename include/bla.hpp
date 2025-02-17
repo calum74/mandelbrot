fcc9c0215b86e04d42790f6a7d719b066598c18e
Calum Grant <calumgrant@github.com>
2025-02-17 17:05:38 +0000

    WIP

--- include/bla.hpp
+++ include/bla.hpp
@@ -115,13 +115,13 @@ template <typename DeltaType> bool is_valid(std::pair<DeltaType, DeltaType> p) {
 }
 
 template <typename TermType> struct jump_terms {
-  TermType A = 1, A2 = 0, B = 0, B2 = 0, C = 0;
+  TermType A = {1}, A2 = {0}, B = {0}, B2 = {0}, C = {0};
 };
 
 template <typename TermType>
 jump_terms<TermType> step_terms(const jump_terms<TermType> &terms, TermType z) {
   return {2 * z * terms.A, 2 * z * terms.A2 + terms.A * terms.A,
-          2 * z * terms.B + 1, 2 * z * terms.B2 + terms.B * terms.B,
+          2 * z * terms.B + TermType{1}, 2 * z * terms.B2 + terms.B * terms.B,
           2 * z * terms.C + 2 * terms.A * terms.B};
 }
 
@@ -133,97 +133,7 @@ public:
   DeltaType d_ik, e_nik, e_mik;
 };
 
-template <typename DeltaType, typename TermType> class linear_step {
-public:
-  linear_step() = default;
 
-  linear_step(DeltaType dc)
-      : A{1}, A2{}, B{}, B2{}, C{}, dc{dc}, dz{}, jZ(0), debug_from(0),
-        debug_to(0) {}
-
-  linear_step restart() const {
-    return {TermType{1}, TermType{0}, TermType{0}, TermType{0}, TermType{0},
-            dc,          dz,          jZ,          debug_to,    debug_to};
-  }
-
-  template <RandomAccessOrbit Reference>
-  linear_step next_iteration(const Reference &reference) const {
-    auto z = reference[jZ];
-    auto orbit_z = z + dz;
-    auto dz_next = 2 * z * dz + dz * dz + dc;
-
-    int jZ_next = jZ + 1;
-
-    if (jZ_next >= reference.size() - 1 || escaped(z) ||
-        fractals::norm(z) < fractals::norm(dz)) {
-      dz_next =
-          reference[jZ_next] + convert<typename Reference::value_type>(dz_next);
-      jZ_next = 0;
-    }
-
-    TermType two_orbit_z = 2 * orbit_z;
-
-    return {two_orbit_z * A,
-            two_orbit_z * A2 + A * A,
-            two_orbit_z * B + TermType{1, 0},
-            two_orbit_z * B2 + B * B,
-            two_orbit_z * C + 2 * A * B,
-            dc,
-            dz_next,
-            jZ_next,
-            debug_from,
-            debug_to + 1};
-  }
-
-  // Returns a dz (relative to the reference orbit)
-  // If the precision is invalid, returns nothing
-  // ?? Return complex NaN?
-  std::optional<DeltaType> jump_dz(DeltaType dz1, DeltaType dc1,
-                                   int expected_from, int expected_to) const {
-    assert(debug_from == expected_from);
-    assert(debug_to == expected_to);
-    auto l = get_local_dz(dz1, dc1 - dc);
-    using T = double;
-
-    if (convert<T>(fractals::norm(B2)) * fractals::norm(dc1 - dc) >
-        1e-15 * convert<T>(fractals::norm(B)))
-      return std::nullopt;
-
-    if (convert<T>(fractals::norm(A2)) * fractals::norm(dz1) >
-        1e-15 * convert<T>(fractals::norm(A)))
-      return std::nullopt;
-
-    // !! Test for escape
-    return is_valid(l) ? std::optional<DeltaType>{convert<DeltaType>(l.first)}
-                       : std::nullopt;
-  }
-
-  template <RandomAccessOrbit Reference>
-  auto get_z(const Reference &reference) const {
-    return reference[jZ] + dz;
-  }
-
-private:
-public: // Temporary
-  linear_step(TermType a, TermType a2, TermType b, TermType b2, TermType c,
-              DeltaType dc, DeltaType dz, int jZ, int from, int to)
-      : A(fractals::normalize(a)), A2(fractals::normalize(a2)),
-        B(fractals::normalize(b)), B2(fractals::normalize(b2)),
-        C(fractals::normalize(c)), dc(dc), dz(dz), jZ(jZ), debug_from(from),
-        debug_to(to) {}
-
-  TermType A, A2, B, B2, C;
-  DeltaType dc, dz;
-  int jZ;                   // Index into the reference orbit
-  int debug_from, debug_to; // Not needed but useful for checking
-
-  // Gets the new dz, relative to the orbit from our dc.
-  // You would need to add dz to get the orbit relative to the reference orbit.
-  // !! TermType (except that it doesn't work!)
-  std::pair<TermType, TermType> get_local_dz(TermType dz1, TermType dc1) const {
-    return {A * dz1 + B * dc1, A2 * dz1 * dz1 + B2 * dc1 * dc1 + C * dc1 * dz1};
-  }
-};
 
 /*
 A bivariate orbit with a fixed step size.
@@ -241,45 +151,69 @@ public:
   // j is the current orbit
   int get(const DeltaType d_jk, int max_iterations) {
 
-    int n=0;
+    int n = 0;
     DeltaType e_njk = 0;
     int reference_index = 0;
 
     using LowPrecisionType = typename Reference::value_type;
     LowPrecisionType z_nj = (*reference_orbit)[reference_index] + e_njk;
 
-    while(!escaped(z_nj) && n < max_iterations)
-    {
-      // Perform once step
+    step new_step;
+
+    while (!escaped(z_nj) && n < max_iterations) {
+      if (n % step_size == 0) {
+        if (n > 0) {
+          int index = n / step_size;
+          new_step.e_mik = e_njk;
+          new_step.m = n;
+
+          // Overwrite/push current term
+          if (index < steps.size())
+            steps[index] = new_step;
+          else
+            steps.push_back(new_step);
+        }
+
+        // See how far we can skip forward
+        // TODO
+
+        // Reset the terms for manual calculation
+        new_step = {.n=n, .d_ik = d_jk, .e_nik = e_njk};
+      }
+
+      // Perform one step
+
+      new_step.terms = step_terms(new_step.terms, TermType(z_nj));
+
       auto z_nk = (*reference_orbit)[reference_index];
 
       // Next iteration
-      e_njk = 2 * z_nk * e_njk + e_njk*e_njk + d_jk;
 
       ++reference_index;
       ++n;
 
+      e_njk = 2 * z_nk * e_njk + e_njk * e_njk + d_jk;
       z_nj = (*reference_orbit)[reference_index] + e_njk;
 
-      if(reference_index >= reference_orbit->size()-1)
-      {
+      if (reference_index >= reference_orbit->size() - 1) {
         reference_index = 0;
         e_njk = z_nj;
       }
     }
 
-    if(n == max_iterations) return 0;
+    if (n == max_iterations)
+      return 0;
 
     return n;
   }
 
-  int get_skipped_iterations() const { return last_jump; }
+  int get_skipped_iterations() const { return skipped_steps; }
 
 private:
   const Reference *reference_orbit;
 
   std::vector<step> steps;
 
-  int last_jump = 0;
+  int skipped_steps = 0;
 };
 } // namespace mandelbrot
