3f65c69c482a9ef5a24f94e05e5ac7123f500c31
Calum Grant <calumgrant@github.com>
2025-02-26 10:10:39 +0000

    Tweak to default parameters

---
+++ doc/shading.md
@@ -0,0 +1,13 @@
+# Rendering the Mandelbrot set
+
+## Smooth shading
+
+## Higher order Mandelbrot sets
+
+## Colour mapping
+
+## Colour gradient
+
+## Variable gradients
+
+## Shading
--- include/shader_parameters.hpp
+++ include/shader_parameters.hpp
@@ -6,15 +6,15 @@ namespace fractals {
     Options that control the rendering of the fractal.
 */
 struct shader_parameters {
-  int colour_scheme =168; // Any integer used to seed the random number generator
+  int colour_scheme = 138; // Any integer used to seed the random number generator
   double colour_gradient = 30; // Iterations per colour
   double colour_offset = 0;
   bool auto_gradient = true;
 
   bool shading = true;
-  double ambient_brightness = 0.4;       // Value between 0 and 1
-  double source_brightness = 0.5;        // Value between 0 and 1
-  double source_direction_radians = 3.14/4; // Value between 0 and 2π
-  double source_elevation_radians = 3.14/4; // Value between 0 and π
+  double ambient_brightness = 0.6;       // Value between 0 and 1
+  double source_brightness = 1.2;        // Value between 0 and 1, or higher for saturation
+  double source_direction_radians = 0.70 * 2 * 3.14; // Value between 0 and 2π
+  double source_elevation_radians = 0.75 * 0.5 * 3.14; // Value between 0 and π
 };
 } // namespace fractals
