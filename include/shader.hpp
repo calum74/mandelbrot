142d3d6bed670a8e70727b7b24acb5a10da71e6a
Calum Grant <calumgrant@github.com>
2025-02-26 11:10:26 +0000

    Refactoring shader

--- include/shader.hpp
+++ include/shader.hpp
@@ -1,4 +1,13 @@
-namespace fractals
-{
-    double shade(double dx, double dy, double source_x, double source_y, double source_z);
-}
+#pragma once
+#include "rgb.hpp"
+#include <vector>
+
+namespace fractals {
+double calculate_brightness(double dx, double dy, double colour_gradient,
+                            double ambient_brightness, double source_brightness,
+                            double source_x, double source_y, double source_z,
+                            double source_length);
+
+std::vector<RGB> generate_colours(int size, int seed);
+
+} // namespace fractals
--- src/shader.cpp
+++ src/shader.cpp
@@ -1,13 +1,43 @@
 #include "shader.hpp"
 #include <cmath>
+#include <random>
 
-double fractals::shade(double dx, double dy, double source_x, double source_y, double source_z)
+double fractals::calculate_brightness(double dx, double dy,
+                                      double colour_gradient,
+                                      double ambient_brightness,
+                                      double source_brightness, double source_x,
+                                      double source_y, double source_z,
+                                      double source_length) {
+  dx /= colour_gradient;
+  dy /= colour_gradient;
+
+  dx *= 3000;
+  dy *= 3000;
+
+  double surface_normal_x = -dx;
+  double surface_normal_y = -dy;
+  double surface_normal_z = 1;
+  double surface_normal_length = std::sqrt(surface_normal_x * surface_normal_x +
+                                           surface_normal_y * surface_normal_y +
+                                           surface_normal_z * surface_normal_z);
+
+  double dot_product =
+      (surface_normal_x * source_x + surface_normal_y * source_y +
+       surface_normal_z * source_z) /
+      (surface_normal_length * source_length);
+
+  if (dot_product < 0)
+    dot_product = 0;
+
+  return ambient_brightness + source_brightness * dot_product;
+}
+
+std::vector<fractals::RGB> fractals::generate_colours(int size, int seed)
 {
-    // Copilot created this:
-    double distance = std::sqrt(dx * dx + dy * dy);
-    double angle = std::atan2(dy, dx);
-    double light_angle = std::atan2(source_y - dy, source_x - dx);
-    double angle_diff = light_angle - angle;
-    double shade = 1.0 - 0.5 * (1.0 + std::cos(angle_diff));
-    return shade;
+  std::mt19937 e(seed);
+  std::vector<RGB> newColours(size);
+  for (auto &c : newColours) {
+    c = e() & 0xffffff;
+  }
+  return newColours;
 }
