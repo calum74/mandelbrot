e3fd9d6a700eb83758871bf23d593068ca40ab72
Calum Grant <calumgrant@github.com>
2025-02-27 07:04:12 +0000

    Refactoring view

--- include/view.hpp
+++ include/view.hpp
@@ -1,38 +1,74 @@
 #pragma once
-#include "pixmap.hpp"
 #include "calculation_metrics.hpp"
 #include "fractal.hpp"
+#include "pixmap.hpp"
 #include "view_coords.hpp"
 #include <atomic>
 #include <future>
 
 namespace fractals {
-// Manages the multithreaded animation and rendering
+/*
+This is currently a work in progress.
+
+Manages the multithreaded calculation and animation
+*/
 class view {
 public:
-  pixmap<error_value<double>> values;
+  view();
+  ~view();
+
+  pixmap<double> values;
 
   class listener {
   public:
-    virtual void redraw() = 0;
-    virtual void update_status(const calculation_metrics &) = 0;
+    virtual void update(const calculation_metrics &) = 0;
+    virtual void animation_timeout(const calculation_metrics &) = 0;
   };
 
   void set_size(int w, int h);
   void set_fractal(const fractal &new_fractal);
-  void set_listener(listener &);
+  void set_listener(listener *);
   void set_coords(const view_coords &new_coords);
+  void set_threading(int threads); // 0 for hardware
+
+  // Performs a smooth zoom in or a zoom out to the specified point.
+  void animate_to(int x, int y, double ratio, double duration);
+
+  // Performs an instant zoom in or out to the given point
+  void zoom(int x, int y, double ratio);
+
+  // Performs an instant scroll to the given point
+  void scroll(int dx, int dy);
+
+  void stop_current_animation_and_set_as_current();
 
 private:
   std::shared_ptr<fractal_calculation> calculation;
   std::shared_ptr<fractal_calculation_factory> fractal;
+  listener *current_listener;
 
-  view_coords current_coords;
+  view_coords current_coords;  // What we're currently calculating
   calculation_metrics metrics;
 
-  std::future<void> calculation_thread;
-  std::future<void> animation_thread;
-  std::atomic<bool> cancel_calculation;
-  std::atomic<bool> cancel_animation;
+  std::future<void> calculation_future;
+  std::future<void> animation_future;
+  std::atomic<bool> stop_calculation;
+  std::atomic<bool> stop_animation;
+  std::condition_variable animation_condition;
+
+  pixmap<error_value<double>> previous_calculation_values, current_calculation_values;
+
+  class my_calculation_pixmap;
+
+  int calculation_threads;
+
+  void start_calculating();
+  void stop_calculating();
+  void start_animating();
+  void stop_animating();
+
+  void animation_thread();
+
+  void complete_layer(double min_depth, double max_depth, std::uint64_t points_calculated, int stride);
 };
 } // namespace fractals
--- src/view.cpp
+++ src/view.cpp
@@ -1,1 +1,83 @@
 #include "view.hpp"
+#include "calculation_pixmap.hpp"
+#include <chrono>
+
+using namespace std::literals::chrono_literals;
+
+fractals::view::view() : current_listener{} {
+  calculation_threads = 4;
+}
+
+fractals::view::~view() {
+  stop_calculating();
+  stop_animating();
+}
+
+void fractals::view::set_listener(listener *l) { current_listener = l; }
+
+void fractals::view::stop_calculating() {
+  if (calculation_future.valid()) {
+    stop_calculation = true;
+    calculation_future.wait();
+    stop_calculation = false;
+  }
+}
+
+struct fractals::view::my_calculation_pixmap
+    : public fractals::calculation_pixmap {
+  fractals::view &view;
+
+  my_calculation_pixmap(fractals::view &v)
+      : fractals::calculation_pixmap(v.current_calculation_values,
+                                     16, *v.calculation), view(v) {}
+
+  virtual void layer_complete(int stride) {
+    view.complete_layer(min_depth, max_depth, points_calculated, stride);
+  }
+};
+
+void fractals::view::start_calculating() {
+  stop_calculating();
+  calculation_future = std::async([&] {
+    my_calculation_pixmap pm(*this);
+    pm.calculate(calculation_threads, stop_calculation);
+  });
+}
+
+void fractals::view::stop_animating() {
+  if (animation_future.valid()) {
+    stop_animation = true;
+    animation_condition.notify_one();
+    animation_future.wait();
+    stop_animation = false;
+  }
+}
+
+void fractals::view::animation_thread() {
+  while (!stop_animation) {
+    std::unique_lock<std::mutex> lock;
+    auto e = animation_condition.wait_for(lock, 10ms);
+
+    if (e == std::cv_status::timeout && !stop_animation) {
+
+      // If we are in the animation time, 
+      // Are we in the animation time
+      // Look at the wallclock time...
+      // Time to
+      // The condition
+
+      // If we have finished animating, then either
+      // a) we have finished calculating
+      // b) we have not finished calculating, and we need to wait for the calculation to finish
+      // c) we haven't finished calculating, and we show the intermediate results straight away.
+      // If we finished calculating, then copy the pixels to the output and we are finished
+      // If we haven't finished calculating, then
+    }
+  }
+}
+
+void fractals::view::complete_layer(double min_depth, double max_depth, std::uint64_t points_calculated, int stride)
+{
+  // If we're not animating, or we have finished animating this step,
+  // copy the pixels over and notify the listener.
+}
