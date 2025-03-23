3f5ecda28675c8435c88f8e7ef3d0eed68f414ee
Calum Grant <calumgrant@github.com>
2025-03-23 12:07:41 +0000

    Refactor shader

--- include/shader.hpp
+++ include/shader.hpp
@@ -18,4 +18,25 @@ std::vector<RGB> generate_colours(int size, int seed);
 
 RGB get_colour_from_index(const std::vector<RGB> &colours, double index, double brightness);
 
+struct view_parameters;
+struct shader_parameters;
+
+class shader {
+public:
+  virtual ~shader() = default;
+  virtual void randomize() = 0;
+  virtual void resetGradient() = 0;
+  virtual void setRange(double min, double max) = 0;
+  virtual void maybeUpdateRange(double min, double max) = 0;
+  virtual RGB operator()(double depth, double dx, double dy) const = 0;
+  virtual RGB operator()(double depth) const = 0;
+
+  virtual void load(const view_parameters &) = 0;
+  virtual void save(view_parameters &) const = 0;
+  virtual void setParameters(const shader_parameters &) = 0;
+  virtual void getParameters(shader_parameters &params) = 0;
+};
+
+std::unique_ptr<shader> make_shader();
+
 } // namespace fractals
